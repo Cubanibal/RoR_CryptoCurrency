4bc141d8b7876dd6b3622b6f489697f22b73f222
Anibal Montero <cubanibal@hotmail.com>
2021-11-08 18:09:45 +0100

    scaffold para crear la bd crypto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_103224) do
+ActiveRecord::Schema.define(version: 2021_11_08_161053) do
+
+  create_table "cryptos", force: :cascade do |t|
+    t.string "simbolo"
+    t.integer "user_id"
+    t.decimal "cost_per"
+    t.decimal "amount_owned"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cryptos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
