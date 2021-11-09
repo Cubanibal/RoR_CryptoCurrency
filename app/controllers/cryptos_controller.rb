0b7c02cb8f92a7d958fbca4b112ef1c02b4a3633
Anibal Montero <cubanibal@hotmail.com>
2021-11-10 00:10:29 +0100

    lista para heroku

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,13 +1,37 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :usuario_correcto, only: %i[ show edit update destroy ]
 
   # GET /cryptos or /cryptos.json
   def index
     @cryptos = Crypto.all
+
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&CMC_PRO_API_KEY=c93b03ea-b0d3-4f24-8908-2c2ac8494af0'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @array1 = JSON.parse(@response).to_a
+    @array2 =@array1[1].to_a
+    
+    @lookup_crypto = @array2[1].to_a
+    @ganapierde = 0
   end
 
   # GET /cryptos/1 or /cryptos/1.json
   def show
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&CMC_PRO_API_KEY=c93b03ea-b0d3-4f24-8908-2c2ac8494af0'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @array1 = JSON.parse(@response).to_a
+    @array2 =@array1[1].to_a
+    
+    @show_crypto = @array2[1].to_a
   end
 
   # GET /cryptos/new
@@ -65,5 +89,10 @@ class CryptosController < ApplicationController
     # Only allow a list of trusted parameters through.
     def crypto_params
       params.require(:crypto).permit(:simbolo, :user_id, :cost_per, :amount_owned)
+    end
+
+    def usuario_correcto
+      @correcto = current_user.cryptos.find_by(id: params[:id])
+      redirect_to cryptos_path, notice: "No está autorizado" if @correcto.nil?
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,10 @@ class HomeController < ApplicationController
     @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&CMC_PRO_API_KEY=c93b03ea-b0d3-4f24-8908-2c2ac8494af0'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
-    @coins = JSON.parse(@response)
+    @array1 = JSON.parse(@response).to_a
+    @array2 =@array1[1].to_a
+    
+    @coins = @array2[1].to_a
     
   end
   def nosotros
