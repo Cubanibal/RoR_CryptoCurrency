ca540a42932456be3db5cacfc42b18a44bf527f3
Anibal Montero <cubanibal@hotmail.com>
2021-11-10 06:46:17 +0100

    formateados números

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :cryptos
   devise_for :users
   root 'home#index'
-  get 'home/nosotros'
   get 'home/lookup'
   post "/home/lookup" => 'home/lookup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
